efb50a8762fc252447de9218180f4f9e97cc5b79
Kiflan Adli <kiflanadli@gmail.com>
2023-01-05 21:11:29 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-slate-100">
+      <header>
+        <h1 className="text-3xl font-bold underline mb-3">Coming Soon</h1>
       </header>
+      <div className="center">This website is under development</div>
     </div>
   );
 }
