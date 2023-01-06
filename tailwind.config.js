9adb77c3e912bb343f719d8f19375a6e329ee1d9
Kiflan Adli <kiflanadli@gmail.com>
2023-01-06 17:02:07 +0700

    config sizing & home background

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,29 +14,25 @@ module.exports = {
         "barlow-condensed": ['"Barlow Condensed"', "sans-serif"],
       },
       fontSize: {
-        sm: [
-          "0.875rem",
-          {
-            letterSpacing: "0.1678em",
-          },
-        ],
-        base: [
-          "1rem",
-          {
-            letterSpacing: "0.1687em",
-          },
-        ],
+        sm: "0.875rem",
+        base: "1rem",
         lg: "1.125rem",
-        "3xl": [
-          "1.75rem",
-          {
-            letterSpacing: "0.1696em",
-          },
-        ],
+        "3xl": "1.75rem",
         "4xl": "2rem",
         "6xl": "3.5rem",
         "8xl": "6.25rem",
         "10xl": "9.375rem",
+      },
+      letterSpacing: {
+        sm: "0.1678em",
+        base: "0.1687em",
+        "3xl": "0.1696em",
+      },
+      backgroundImage: {
+        "home-mobile": "url('/public/assets/home/background-home-mobile.jpg')",
+        "home-tablet": "url('/public/assets/home/background-home-tablet.jpg')",
+        "home-desktop":
+          "url('/public/assets/home/background-home-desktop.jpg')",
       },
     },
   },
