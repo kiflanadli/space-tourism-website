afeb9ed192d6b61cae4797866fa6bff8f51ddc39
Kiflan Adli <kiflanadli@gmail.com>
2023-01-06 11:22:15 +0700

    setup theme style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black-russian": "#0b0d17",
+        quartz: "#d0d6f9",
+      },
+      fontFamily: {
+        bellefair: ['"Bellefair"', "serif"],
+        barlow: ['"Barlow"', "sans-serif"],
+        "barlow-condensed": ['"Barlow Condensed"', "sans-serif"],
+      },
+      fontSize: {
+        sm: [
+          "0.875rem",
+          {
+            letterSpacing: "0.1678em",
+          },
+        ],
+        base: [
+          "1rem",
+          {
+            letterSpacing: "0.1687em",
+          },
+        ],
+        lg: "1.125rem",
+        "3xl": [
+          "1.75rem",
+          {
+            letterSpacing: "0.1696em",
+          },
+        ],
+        "4xl": "2rem",
+        "6xl": "3.5rem",
+        "8xl": "6.25rem",
+        "10xl": "9.375rem",
+      },
+    },
   },
   plugins: [],
 };
