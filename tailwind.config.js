88d5777e5c34753c6157b5a5cb24a0c90a6bedbc
Kiflan Adli <kiflanadli@gmail.com>
2023-01-07 22:12:41 +0700

    add backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,22 @@ module.exports = {
         "home-tablet": "url('/public/assets/home/background-home-tablet.jpg')",
         "home-desktop":
           "url('/public/assets/home/background-home-desktop.jpg')",
+        "destination-mobile":
+          "url('/public/assets/destination/background-destination-mobile.jpg')",
+        "destination-tablet":
+          "url('/public/assets/destination/background-destination-tablet.jpg')",
+        "destination-desktop":
+          "url('/public/assets/destination/background-destination-desktop.jpg')",
+        "crew-mobile": "url('/public/assets/crew/background-crew-mobile.jpg')",
+        "crew-tablet": "url('/public/assets/crew/background-crew-tablet.jpg')",
+        "crew-desktop":
+          "url('/public/assets/crew/background-crew-desktop.jpg')",
+        "technology-mobile":
+          "url('/public/assets/technology/background-technology-mobile.jpg')",
+        "technology-tablet":
+          "url('/public/assets/technology/background-technology-tablet.jpg')",
+        "technology-desktop":
+          "url('/public/assets/technology/background-technology-desktop.jpg')",
       },
     },
   },
