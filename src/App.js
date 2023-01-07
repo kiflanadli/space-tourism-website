60e21a9ea8f179b8bfd8ffc4fd659a9ece6342ca
Kiflan Adli <kiflanadli@gmail.com>
2023-01-07 12:56:42 +0700

    add home page & adjustment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Navigation from "./Components/Navigation";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { MainNavigation } from "./Components/Navigation";
+import Home from "./Views/Home";
 
 function App() {
   return (
     <div className="box-border font-barlow text-white uppercase min-h-screen p-6 bg-no-repeat bg-cover bg-home-mobile sm:bg-home-tablet md:bg-home-desktop">
       <Router>
-        <header className="min-h-[7rem]">
-          <Navigation />
-        </header>
-        <Switch>
-          <Route path="/" exact>
-            home
-          </Route>
-          <Route path="/destination">destination</Route>
-          <Route path="/crew">crew</Route>
-          <Route path="/technology">technology</Route>
-        </Switch>
+        <MainNavigation />
+        <main className="mt-7">
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/destination">destination</Route>
+            <Route path="/crew">crew</Route>
+            <Route path="/technology">technology</Route>
+          </Switch>
+        </main>
       </Router>
     </div>
   );
---
+++ src/Views/Home.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { Heading1, Heading5 } from "../Components/Headings";
+import { BodyText } from "../Components/Texts";
+
+export default function Home() {
+  return (
+    <div className="text-center flex flex-col justify-around min-h-[80vh]">
+      <div>
+        <Heading5>So, you want to travel to</Heading5>
+        <Heading1>Space</Heading1>
+        <BodyText>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </BodyText>
+      </div>
+      <div>
+        <Link to="/destination">
+          <button className="rounded-full bg-white w-[45%] aspect-square text-black-russian uppercase text-xl tracking-[0.063rem] font-bellefair">
+            Explore
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
