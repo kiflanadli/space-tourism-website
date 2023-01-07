e18957b7db4334c4c29a002d05d001f1a81c9278
Kiflan Adli <kiflanadli@gmail.com>
2023-01-07 10:25:22 +0700

    fix naming convention

--- src/Components/Headings.js
+++ src/Components/Headings.js
@@ -1,20 +1,20 @@
-export function Heading_1(props) {
+export function Heading1(props) {
   return <h1 className="font-bellefair font-10xl">{props.children}</h1>;
 }
 
-export function Heading_2(props) {
+export function Heading2(props) {
   return <h2 className="font-bellefair font-8xl">{props.children}</h2>;
 }
 
-export function Heading_3(props) {
+export function Heading3(props) {
   return <h3 className="font-bellefair font-6xl">{props.children}</h3>;
 }
 
-export function Heading_4(props) {
+export function Heading4(props) {
   return <p className="font-bellefair font-4xl">{props.children}</p>;
 }
 
-export function Heading_5(props) {
+export function Heading5(props) {
   return (
     <p className="font-barlow-condensed font-3xl tracking-3xl">
       {props.children}
@@ -22,11 +22,11 @@ export function Heading_5(props) {
   );
 }
 
-export function SubHeading_1(props) {
+export function SubHeading1(props) {
   return <p className="font-bellefair font-3xl">{props.children}</p>;
 }
 
-export function SubHeading_2(props) {
+export function SubHeading2(props) {
   return (
     <p className="font-barlow-condensed font-sm tracking-sm">
       {props.children}
