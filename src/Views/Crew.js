442e93c9e05f0290d32a860100732a0b5e71c7e4
Kiflan Adli <kiflanadli@gmail.com>
2023-01-07 22:14:47 +0700

    add crew & technology pages

---
+++ src/Views/Crew.js
@@ -0,0 +1,5 @@
+export default function Crew() {
+  return (
+    <div className="text-center p-6 pt-20 min-h-screen bg-no-repeat bg-cover bg-crew-mobile sm:bg-crew-tablet md:bg-crew-desktop"></div>
+  );
+}
---
+++ src/Views/Technology.js
@@ -0,0 +1,5 @@
+export default function Technology() {
+  return (
+    <div className="text-center p-6 pt-20 min-h-screen bg-no-repeat bg-cover bg-technology-mobile sm:bg-technology-tablet md:bg-technology-desktop"></div>
+  );
+}
